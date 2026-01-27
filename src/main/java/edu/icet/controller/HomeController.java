1d279579742ebdbf75f773598c4ddabe7168fcc1
Maalik_Hassan <maalikhassan1@gmail.com>
2026-01-28 00:57:12 +0530

    UI: Implemented Vertical Box sidebar for main navigation. and add empty classes necessary for loading that initial UI

--- src/main/java/edu/icet/controller/HomeController.java
+++ src/main/java/edu/icet/controller/HomeController.java
@@ -1,4 +1,42 @@
 package edu.icet.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
+
 public class HomeController {
+    @FXML
+    private AnchorPane contentPane; // The ID from your FXML
+
+    public void loadCustomerView(ActionEvent event) throws IOException {
+        setNode("CustomerForm.fxml");
+    }
+
+    public void loadItemView(ActionEvent event) throws IOException {
+        setNode("ItemForm.fxml");
+    }
+
+    // A helper method to keep your code clean and dry
+    private void setNode(String fxmlPath) throws IOException {
+        Node node = FXMLLoader.load(getClass().getResource("/view/" + fxmlPath));
+        contentPane.getChildren().setAll(node); // This swaps the content instantly
+
+        // Ensure the sub-view fills the whole pane
+        AnchorPane.setTopAnchor(node, 0.0);
+        AnchorPane.setBottomAnchor(node, 0.0);
+        AnchorPane.setLeftAnchor(node, 0.0);
+        AnchorPane.setRightAnchor(node, 0.0);
+    }
+
+    public void loadDashboardView(ActionEvent actionEvent) {
+
+    }
+
+    public void loadOrderView(ActionEvent actionEvent) {
+
+    }
 }
