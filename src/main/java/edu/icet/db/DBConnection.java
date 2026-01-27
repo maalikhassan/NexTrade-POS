35207881c7c7b3c4066a6be2ffce64dcba2cdb4c
Maalik_Hassan <maalikhassan1@gmail.com>
2026-01-27 23:57:11 +0530

    add DB connection class for singleton design pattern impl

---
+++ src/main/java/edu/icet/db/DBConnection.java
@@ -0,0 +1,25 @@
+package edu.icet.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    private static DBConnection dbConnection;
+    private Connection connection;
+
+    private DBConnection() throws SQLException {
+        // Replace 'thogakade' with your actual DB name from the script
+        connection = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/Thogakade", "root", "mysqllol@123"
+        );
+    }
+
+    public static DBConnection getInstance() throws SQLException {
+        return (dbConnection == null) ? dbConnection = new DBConnection() : dbConnection;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+}
