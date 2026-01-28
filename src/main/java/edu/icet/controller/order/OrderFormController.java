2197445305d1184dd2c63e1da2013e06ce83b598
Maalik_Hassan <maalikhassan1@gmail.com>
2026-01-28 16:08:47 +0530

    UI: Improve design and layout arrangement for better UI UX

--- src/main/java/edu/icet/controller/order/OrderFormController.java
+++ src/main/java/edu/icet/controller/order/OrderFormController.java
@@ -1,4 +1,75 @@
 package edu.icet.controller.order;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class OrderFormController {
+
+    @FXML
+    private Button btnAddToCart;
+
+    @FXML
+    private Button btnPlaceOrder;
+
+    @FXML
+    private TableColumn<?, ?> colDescription;
+
+    @FXML
+    private TableColumn<?, ?> colDiscount;
+
+    @FXML
+    private TableColumn<?, ?> colItemCode;
+
+    @FXML
+    private TableColumn<?, ?> colQuantity;
+
+    @FXML
+    private TableColumn<?, ?> colTotal;
+
+    @FXML
+    private TableColumn<?, ?> colUnitPrice;
+
+    @FXML
+    private Label lblNetTotal;
+
+    @FXML
+    private TableView<?> tblCartItems;
+
+    @FXML
+    private TextField txtCusId;
+
+    @FXML
+    private TextField txtCusName;
+
+    @FXML
+    private TextArea txtDescription;
+
+    @FXML
+    private TextField txtDiscount;
+
+    @FXML
+    private TextField txtItemCode;
+
+    @FXML
+    private TextField txtQty;
+
+    @FXML
+    private TextField txtUnitPrice;
+
+    @FXML
+    void btnAddToCartAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnPlaceOrderAction(ActionEvent event) {
+
+    }
+
 }
---
+++ src/main/java/edu/icet/repository/order/OrderRepositoryImpl.java
@@ -0,0 +1,4 @@
+package edu.icet.repository.order;
+
+public class OrderRepositoryImpl implements OrderRepository{
+}
